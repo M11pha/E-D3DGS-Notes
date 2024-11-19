c7e1afc0a1e9529897dcd5a647d0622121f6c664
Mipha <cyforever1@gmail.com>
2024-11-19 23:05:54 +0800

    Update downsample_point.py

--- script/downsample_point.py
+++ script/downsample_point.py
@@ -1,3 +1,13 @@
+# cook_spinach原始colmap生成的点云数量924016,降采样后为93172
+# 使用方法----------------------------------------------------------------------------
+# python script/downsample_point.py \
+# <location>/<scene>/colmap/dense/workspace/fused.ply \
+# <location>/<scene>/points3D_downsample.ply
+# 
+# $ python script/downsample_point.py \
+# /home/ekko/datasets/Neural_3D_Video/cook_spinach/colmap/dense/workspace/fused.ply \
+# /home/ekko/datasets/Neural_3D_Video/cook_spinach/points3D_downsample.ply
+# 
 import open3d as o3d
 import sys
 def process_ply_file(input_file, output_file):
@@ -6,6 +16,7 @@ def process_ply_file(input_file, output_file):
 
     voxel_size=0.001
     while len(pcd.points) > 100000:
+        # https://www.open3d.org/docs/0.14.1/tutorial/geometry/pointcloud.html
         pcd = pcd.voxel_down_sample(voxel_size=voxel_size)
         print(f"Downsampled points: {len(pcd.points)}")
         voxel_size+=0.005
